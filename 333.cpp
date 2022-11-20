ac9f740f27dc2dd1e6aa661c725c823201c15066
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-20 15:10:24 +0300

    Add files via upload

---
+++ 333.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	string s;
+	int n;
+	int maxL = 0;
+	string masS;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s;
+		int c = s.size();
+		if (c > maxL) {
+			maxL = c;
+			masS = s;
+		}
+	}
+	cout << masS;
+
+
+
+}
+
